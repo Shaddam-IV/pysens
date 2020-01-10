942d5c2f41c0c7026df2e90377259f4fae8f5926
Shaddam-IV <59708937+Shaddam-IV@users.noreply.github.com>
2020-01-09 19:21:55 -0700

    Add files via upload
    New features:
    Outputs the sensitivity as cm/360 or in-game setting
    Takes into account user DPI

--- pysens.py
+++ pysens.py
@@ -4,24 +4,58 @@
 
 import random
 
-# declaring variables
+# variables
 system = 'metric' # metric or imperial
-dpi = 800 # mouse DPI
-game = 'ow' # the game to generate sensitvity for
-minsens = 20 # minimum sensitivity (in cm/360)
-maxsens = 50 # maximum sensitivity (in cm/360)
+DPI = 800 # mouse DPI
+game = 'Overwatch' # the game to generate sensitvity for
+OverwatchYaw = 0.0066
+MinSens = 20 # minimum sensitivity (in cm/360)
+MaxSens = 50 # maximum sensitivity (in cm/360)
+SelectedYaw = 0
+output = 'game' # output in either physical (cm/360) or game setting
+
+if game == 'Overwatch':
+
+	SelectedYaw = OverwatchYaw
 
 # cm/360 sens generation
 # getting a random float between min and max variables, then rounding it to 2 decimal places
-gen360 = (round(random.uniform(minsens,maxsens), 2))
-# converting to string to print
-sgen360 = str(gen360)
+RandomFloat = (random.uniform(MinSens,MaxSens))
+RandomFloatRounded = (round(RandomFloat, 2))
 
+# converting rounded float to string in order to print
+RandomFloatRoundedString = str(RandomFloatRounded)
 
-# ow sens generation
-# getting the random float from cm/360 generation, will be converted into ow sensitivity using player's DPI
-genow = (gen360)
+# getting DPI before calculations
+DPI = input('DPI? Defaults to 800\n')
 
-# printing cm/360 generated sens
-# gonna replace this with an if to choose ow or cm/360 for the next version
-print (sgen360 + ' cm/360')
+# testing if DPI is integer or not. I know this is probaly extremely sloppy
+if DPI == '':
+	DPI = 800
+else:
+	try:
+		DPI = int(DPI)
+	except ValueError:
+		print ('Please use intergers only')
+	finally:
+		DPI = int(DPI)
+
+# converting float to ow sensitivity
+OverwatchRandomFloat = (4572 / (5 * RandomFloat * DPI * SelectedYaw))
+OverwatchRandomFloatRounded = (round(OverwatchRandomFloat, 2))
+
+# converting rounded float to string in order to print
+OverwatchRandomFloatRoundedString = str(OverwatchRandomFloatRounded)
+
+# asking user for desired output
+
+output = input("cm/360 or Overwatch output? (enter 'cm' or 'ow') Defaults to game\n")
+
+# printing generated sens
+
+if output == 'cm':
+	print (RandomFloatRoundedString + ' cm/360')
+elif output == 'ow' or output == '':
+	print (OverwatchRandomFloatRoundedString + ' In Overwatch settings')
+else:
+	print ('you done fucked up')
